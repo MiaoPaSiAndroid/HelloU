f49ee560bc8b1eef48356dbe75b838aec8f93f24
朱玉辉(EX-ZHUYUHUI002) <EX-ZHUYUHUI002@pingan.com.cn>
2021-02-25 10:39:15 +0800

    本地图片使用 ListView简单使用

--- app/src/main/java/com/miaopasi/hellou/MainActivity.java
+++ app/src/main/java/com/miaopasi/hellou/MainActivity.java
@@ -3,12 +3,44 @@ package com.miaopasi.hellou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<ItemStyle> fruitList = new ArrayList<ItemStyle>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFruits(); // 初始化水果数据
+        MainAdapter adapter = new MainAdapter(MainActivity.this, R.layout.activity_main_item, fruitList);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+
+    private void initFruits() {
+        ItemStyle apple = new ItemStyle("Apple", R.mipmap.takepicguide_picsample_0);
+        fruitList.add(apple);
+        ItemStyle banana = new ItemStyle("Banana", R.mipmap.takepicguide_picsample_1);
+        fruitList.add(banana);
+        ItemStyle orange = new ItemStyle("Orange", R.mipmap.takepicguide_picsample_2);
+        fruitList.add(orange);
+        ItemStyle watermelon = new ItemStyle("Watermelon", R.mipmap.takepicguide_picsample_3);
+        fruitList.add(watermelon);
+        ItemStyle pear = new ItemStyle("Pear", R.mipmap.takepicguide_picsample_4);
+        fruitList.add(pear);
+        ItemStyle grape = new ItemStyle("Grape", R.mipmap.takepicguide_picsample_5);
+        fruitList.add(grape);
+        ItemStyle pineapple = new ItemStyle("Pineapple", R.mipmap.takepicguide_picsample_6);
+        fruitList.add(pineapple);
+        ItemStyle strawberry = new ItemStyle("Strawberry", R.mipmap.takepicguide_picsample_7);
+        fruitList.add(strawberry);
+        ItemStyle cherry = new ItemStyle("Cherry", R.mipmap.takepicguide_picsample_10);
+        fruitList.add(cherry);
+        ItemStyle mango = new ItemStyle("Mango", R.mipmap.takepicguide_picsample_12);
+        fruitList.add(mango);
     }
 }
---
+++ app/src/main/java/com/miaopasi/hellou/MainAdapter.java
@@ -0,0 +1,35 @@
+package com.miaopasi.hellou;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class MainAdapter extends ArrayAdapter {
+    private final int resourceId;
+    public MainAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<ItemStyle> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ItemStyle fruit = (ItemStyle) getItem(position); // 获取当前项的Fruit实例
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, null);//实例化一个对象
+
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);//获取该布局内的图片视图
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);//获取该布局内的文本视图
+        fruitImage.setImageResource(fruit.getImageId());//为图片视图设置图片资源
+        fruitName.setText(fruit.getTitle());//为文本视图设置文本内容
+        return view;
+    }
+}
